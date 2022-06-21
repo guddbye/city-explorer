e270029e13ad1688c59065d96acd02851994cd23
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-06-21 01:45:54 -0700

    Made a form.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    city:'',
+    cityFind: []
+
+  }}
+
+  handleCityInput = (e) => {
+    e.preventDefault();
+    this.setState({
+      city: e.target.value
+    })
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    let url = (`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+    let cityInfo = await axios.get(url);
+  
+    console.log(cityInfo.data);
+  }
+
+  render() {
+
+    return(
+      <>
+      <h1>Explore The City Of Your Next Adventure!</h1>
+      <form onSubmit={this.handleSubmit} >
+        <label> Enter your city: 
+          <input type="text" onInput={this.handleCityInput}/>
+        </label>
+        <button type="submit">EXPLORE!</button>
+
+      </form>
+      {this.state.cityFind && <p>{this.state.cityFind.display_name}</p>}
+      </>
+    )
+  }
+};
+
 
 export default App;
