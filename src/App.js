49067e174f76ea576a33547c4f27b01cc87c37cb
Brentice Loper <54426613+guddbye@users.noreply.github.com>
2022-06-21 22:31:52 -0700

    Made a form and changed a lot of code.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,69 @@
-import './App.css';
 import React from 'react';
 import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    city:'',
-    cityFind: []
-
-  }}
-
-  handleCityInput = (e) => {
-    e.preventDefault();
-    this.setState({
-      city: e.target.value
-    })
+    this.state= {
+      city: '',
+      cityData: {},
+      error: false,
+      errorMessage: ''
+    };
   }
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
-    let url = (`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
-    let cityInfo = await axios.get(url);
-  
-    console.log(cityInfo.data);
+  handleCityInput = (e) => {
+    this.setState({
+      city: e.target.value
+    });
+  };
+
+  handleCitySubmit = async (e) => {
+    e.preventDefault();
+    try{    
+
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+      let cityInfo = await axios.get(url);
+      let cityMap = await `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityInfo.data[0].lat},${cityInfo.data[0].lon}&zoom=10`;
+      console.log(cityInfo.data);
+      this.setState({
+        cityData: cityInfo.data[0],
+        cityMap: cityMap
+      });
+    }
+    catch(error){
+      this.setState({
+        error: true,
+        errorMessage: `An Error has occurred! ${error.response.status} Please try again!`
+      })
+    }
   }
 
   render() {
 
-    return(
+    return( 
       <>
-      <h1>Explore The City Of Your Next Adventure!</h1>
-      <form onSubmit={this.handleSubmit} >
-        <label> Enter your city: 
-          <input type="text" onInput={this.handleCityInput}/>
-        </label>
-        <button type="submit">EXPLORE!</button>
-
-      </form>
-      {this.state.cityFind && <p>{this.state.cityFind.display_name}</p>}
+        <h1>Find a City</h1>
+        <form onSubmit={this.handleCitySubmit}>
+          <label>Enter a City: <br/>
+          <br/>
+            <input type="text" onInput={this.handleCityInput} /> 
+          </label>
+          <button>Go</button>
+        </form>
+        {this.state.error?<Alert variant="danger">{this.state.errorMessage}</Alert>:
+        <Card>
+          <Card.Text>City: {this.state.cityData.display_name}</Card.Text>
+          <Card.Text>Latitude: {this.state.cityData.lat}</Card.Text>
+          <Card.Text>Longitude: {this.state.cityData.lon}</Card.Text>
+          <Card.Img src={this.state.cityMap}/>
+        </Card>}
       </>
-    )
-  }
-};
-
+    )  
+  };
+}
 
 export default App;
