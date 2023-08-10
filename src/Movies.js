979ab904981b56af07571e8a69ea939c8ff4a31e
Brentice Loper <brentice@icloud.com>
2023-08-10 15:34:01 -0700

    Finished product. Need to edit css.

--- src/Movies.js
+++ src/Movies.js
@@ -24,9 +24,6 @@ class SingleMovie extends React.Component {
       <Card>
         <Card.Text>Title: {this.props.movie.title}</Card.Text>
         <Card.Text>Overview: {this.props.movie.overview}</Card.Text>
-        <Card.Text>Average Votes: {this.props.movie.average_votes}</Card.Text>
-        <Card.Text>Total Votes: {this.props.movie.total_votes}</Card.Text>
-        <Card.Text>Popularity: {this.props.movie.popularity}</Card.Text>
         <Card.Text>Released: {this.props.movie.released_on}</Card.Text>
         <Card.Img
           src={`https://image.tmdb.org/t/p/w500${this.props.movie.image_url}`}
